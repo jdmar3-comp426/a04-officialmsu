33b2b6b2b3e348913f09f5991a9d3e18c0dc9307
officialmsu <mhsudeveloper@gmail.com>
2021-11-19 18:11:58 -0500

    Update server.js

--- server.js
+++ server.js
@@ -45,9 +45,9 @@ app.get("/app/update/user/:id", (req, res) => {
 	res.status(200).json({"message":"1 record deleted: ID " + req.body.user.params.id + " (200)" });
 });
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
-app.get("/app/delete/user/:id", (req, res) => {	
-	const stmt = db.prepare("DELETE FROM userinfo WHERE id = ?").run([req.params.id]);
-	res.json({"message":"1 record deleted: ID 2 (200)" });
+app.delete("/app/delete/user/:id", (req, res) => {	
+	const stmt = db.prepare("DELETE FROM userinfo WHERE id = ?").run(req.params.id);
+	res.json({"message":"1 record deleted: ID " + req.params.id + " (200)" });
 });
 // Default response for any other request
 app.use(function(req, res){
