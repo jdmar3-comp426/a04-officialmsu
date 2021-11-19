7679eb1f7e58328f7681fdb04f4d730d398e1c16
officialmsu <mhsudeveloper@gmail.com>
2021-11-19 18:24:42 -0500

    Update server.js

--- server.js
+++ server.js
@@ -24,8 +24,8 @@ app.get("/app/", (req, res, next) => {
 // Define other CRUD API endpoints using express.js and better-sqlite3
 // CREATE a new user (HTTP method POST) at endpoint /app/new/
 app.post("/app/new/", (req, res) => {	
-	const stmt = db.prepare("INSERT INTO userinfo (user, pass) VALUES (?, ?)").run(req.body.user);
-	res.status(200).json(stmt);
+	const stmt = db.prepare("INSERT INTO userinfo (user, pass) VALUES (?, ?)").run(req.body.user, md5(req.body.pass));
+	res.json({"message":"1 record created: ID " + req.params.id + " (201)" });
 });
 
 // READ a list of all users (HTTP method GET) at endpoint /app/users/
