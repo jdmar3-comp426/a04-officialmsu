332af72b3399b2f95155464bfc87f19940b0226e
officialmsu <mhsudeveloper@gmail.com>
2021-11-18 21:16:27 -0500

    Update server.js

--- server.js
+++ server.js
@@ -31,7 +31,10 @@ app.get("/app/users", (req, res) => {
 });
 
 // READ a single user (HTTP method GET) at endpoint /app/user/:id
-
+app.get("/app/user/:id", (req, res) => {	
+	const stmt = db.prepare("SELECT * FROM userinfo WHERE id = " + res.id);
+	res.status(200).json(stmt);
+});
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
 
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
