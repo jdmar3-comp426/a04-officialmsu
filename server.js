00bdf3701ba4081c7759a26925678d9577ef1095
officialmsu <mhsudeveloper@gmail.com>
2021-11-18 21:19:13 -0500

    Update server.js

--- server.js
+++ server.js
@@ -32,13 +32,19 @@ app.get("/app/users", (req, res) => {
 
 // READ a single user (HTTP method GET) at endpoint /app/user/:id
 app.get("/app/user/:id", (req, res) => {	
-	const stmt = db.prepare("SELECT * FROM userinfo WHERE id = " + res.id).get();
+	const stmt = db.prepare("SELECT * FROM userinfo WHERE id = " + req.params.id).get();
 	res.status(200).json(stmt);
 });
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
-
+app.get("/app/update/user/:id", (req, res) => {	
+	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?,user), pass = COALESCE(?,pass) WHERE id = " + req.params.id).get();
+	res.status(200).json(stmt);
+});
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
-
+app.get("/app/delete/user/:id", (req, res) => {	
+	const stmt = db.prepare("DELETE FROM userinfo WHERE id = " + req.params.id).get();
+	res.status(200).json(stmt);
+});
 // Default response for any other request
 app.use(function(req, res){
 	res.json({"message":"Endpoint not found. (404)"});
