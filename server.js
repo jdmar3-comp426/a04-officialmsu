e5a35363c6ac52e46a5d112efa4d2d212ab20236
officialmsu <mhsudeveloper@gmail.com>
2021-11-19 18:33:22 -0500

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.listen(HTTP_PORT, () => {
 });
 // READ (HTTP method GET) at root endpoint /app/
 app.get("/app/", (req, res, next) => {
-    res.json({"message":"Your API is working! (200)"});
+    res.json({"message":"Your API works! (200)"});
 	res.status(200);
 });
 
